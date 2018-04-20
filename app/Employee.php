550264a7b6310c8303a33ef5bb5bfbb3eb74a3e2
Raimund Ege <raimund@ege.com>
2018-04-20 18:28:30 -0500

    Project/Employee realtionship

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    /**
+     * The projects that the employee belongs to.
+     */
+    public function projects()
+    {
+        return $this->belongsToMany('App\Project');
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+     /**
+     * The employees that the belong to this  project.
+     */
+    public function employees()
+    {
+        return $this->belongsToMany('App\Employee');
+    }
 }
