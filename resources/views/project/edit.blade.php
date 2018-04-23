dbcc9d7e21bd430f9355f3e91ef7ac8d9c82a163
Raimund Ege <raimund@ege.com>
2018-04-23 15:38:56 -0500

    projectedit layout

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -5,16 +5,17 @@
     
     <h1 class="my-3">Project Management</h1>
     <div class="row">
-        <div class="col-6">    <h3 class="my-3">Update Project Data</h3>
+        <div class="col-6">
+            <h3 class="my-3">Update Project Data</h3>
     
             <form method="POST" action="{{ '/projupdate/' . $project->id }}">
                 {{ csrf_field() }}
                 <div class="form-group col-8 form-row">
-                    <label for="title" class="col-2">Title</label>
+                    <label for="title" class="col-3">Title</label>
                     <input type="text" class="form-control col" id="title" name="title" value="{{ $project->title }}" required>
                 </div>
                 <div class="form-group col-8 form-row">
-                    <label for="expense" class="col-2">Expense</label>
+                    <label for="expense" class="col-3">Expense</label>
                     <input type="text" class="form-control col" id="expense" name="expense" value="{{ $project->expense }}" required>
                 </div>
                 <input type="submit" value="Update">
